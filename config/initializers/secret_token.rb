9faed00cf235cb533ab53c66ab114ea8b5ad25a8
David Matthews <predsfan26@gmail.com>
2011-09-01 11:18:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollegeMoap::Application.config.secret_token = '0f71dcc176b3d059513c2a23b3a4d2b2cc746211a3c4fad5ca2f7af4aec67b2731ccda37750608507432dfbd53c92634b408f83b37305db56dba8a1d84fb0f87'
